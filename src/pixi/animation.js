c0ca8d21981384ad559e96859689170de026cebd
yfang6 <yfang6@trip.com>
2022-10-16 17:01:38 +0800

    first commit

---
+++ src/pixi/animation.js
@@ -0,0 +1,21 @@
+const PIXI = window.PIXI;
+
+class Animation {
+  constructor(option) {
+    
+  }
+
+  setStage() {
+
+  }
+
+  addLoader() {
+
+  }
+
+  init() {
+
+  }
+}
+
+export default Animation;
